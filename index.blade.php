c7866ce37eb43205dc77f39f852357f95db47a79
dinaelkhiyary12-crypto <dinaelkhiyary12@gmail.com>
2026-02-13 14:03:55 +0100

    view.speaker

--- index.blade.php
+++ index.blade.php
@@ -1,29 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<h3>Participants</h3>
+<h3>Speakers</h3>
 
-<a href="{{ route('participants.create') }}">Add Participant</a><br><br>
+<a href="{{ route('speakers.create') }}">Add Speaker</a><br><br>
 
 <table border="1" cellpadding="5" cellspacing="0">
     <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
+        <th>Name</th>
         <th>Email</th>
-        <th>Phone</th>
         <th>Actions</th>
     </tr>
 
-    @forelse($participants as $participant)
+    @forelse($speakers as $speaker)
         <tr>
-            <td>{{ $participant->first_name }}</td>
-            <td>{{ $participant->last_name }}</td>
-            <td>{{ $participant->email }}</td>
-            <td>{{ $participant->phone }}</td>
+            <td>{{ $speaker->name }}</td>
+            <td>{{ $speaker->email }}</td>
             <td>
-                <a href="{{ route('participants.edit', $participant->id) }}">Edit</a>
+                <a href="{{ route('speakers.edit', $speaker->id) }}">Edit</a>
 
-                <form action="{{ route('participants.destroy', $participant->id) }}" method="POST" style="display:inline;">
+                <form action="{{ route('speakers.destroy', $speaker->id) }}" method="POST" style="display:inline;">
                     @csrf
                     @method('DELETE')
                     <button type="submit" onclick="return confirm('Are you sure?')">Delete</button>
@@ -32,7 +28,7 @@
         </tr>
     @empty
         <tr>
-            <td colspan="5">No participants found.</td>
+            <td colspan="3">No speakers found.</td>
         </tr>
     @endforelse
 </table>
