9239c4e133080a6331702b0a5fbbd8607fe992ef
dinaelkhiyary12-crypto <dinaelkhiyary12@gmail.com>
2026-02-13 14:13:19 +0100

    Remove Arabic comments from SpeakerFactory methods
    Removed comments in Arabic from the SpeakerFactory methods.

--- SpeakerFactory.php
+++ SpeakerFactory.php
@@ -12,20 +12,17 @@ class SpeakerController extends Controller
         $this->middleware('auth');
     }
 
-    // عرض قائمة المتحدثين
     public function index()
     {
         $speakers = Speaker::all();
         return view('speakers.index', ['speakers' => $speakers]);
     }
 
-    // إظهار فورم إنشاء متحدث
     public function create()
     {
         return view('speakers.create');
     }
 
-    // حفظ متحدث جديد
     public function store(Request $request)
     {
         $request->validate([
@@ -43,19 +40,16 @@ class SpeakerController extends Controller
         return redirect()->route('speakers.index')->with('success', 'Speaker created successfully.');
     }
 
-    // عرض تفاصيل المتحدث
     public function show(Speaker $speaker)
     {
         return view('speakers.show', ['speaker' => $speaker]);
     }
 
-    // إظهار فورم تعديل المتحدث
     public function edit(Speaker $speaker)
     {
         return view('speakers.edit', ['speaker' => $speaker]);
     }
 
-    // حفظ التعديلات على المتحدث
     public function update(Request $request, Speaker $speaker)
     {
         $request->validate([
@@ -72,7 +66,6 @@ class SpeakerController extends Controller
         return redirect()->route('speakers.index')->with('success', 'Speaker updated successfully.');
     }
 
-    // حذف المتحدث
     public function destroy(Speaker $speaker)
     {
         $speaker->delete();
